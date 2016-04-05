2434573ce825731439cdb4a99351e5147b7fc6db
Minh Quan Nguyen <minhquanng08@gmail.com>
2016-04-05 00:09:02 -0700

    Thread all incoming requests
    All RPC messages that yt_player receives are now placed inside the job
    queue to be handled by a thread. The hope is to be a little more
    responsive.
    
    Fixes a couple ajax errors. The now playing request was unnecessarily
    re-registering callbacks and causing a loop of callbacks to occur.
    Adding a new song wasn't re-registering the callback on the queue title.

--- backend/yt_player.py
+++ backend/yt_player.py
@@ -26,7 +26,7 @@ class yt_player:
         self._now_playing = ("None","0")
 
         # Log video submissions
-        # self._log = open(file="submissions.txt", mode="a", buffering=1)
+        self._log = open(file="submissions.md", mode="a", buffering=1)
 
         # callback table to handle rpc
         self._callbacks = {
@@ -36,57 +36,51 @@ class yt_player:
         }
 
         # spawn some threads
-        for i in range(5):
+        for i in range(8):
             t = threading.Thread(target=self._thread_task)
             t.daemon = True
             t.start()
 
     def _thread_task(self):
         """
-        Threading task. Requesting the video name and id can take a while so the
-        job is offloaded to a thread. A submitted video link is added to a job
-        queue. A thread will wait on a condition variable to pull a link from
-        the job queue. Once a link is pulled from the job queue, it will request
-        the metadata and then do an atomic add to the video queue.
+        Threading task. All requests are placed in the job queue where threads
+        pull one off and process the command.
         """
         while True:
             self._jobcv.acquire()
             while len(self._jobq) == 0:
                 self._jobcv.wait()
-            link = self._jobq.popleft()
+            print( "{} reporting in".format(threading.get_ident() % 100))
+            sock, parsed_json = self._jobq.popleft()
             self._jobcv.release()
 
-            complete = subprocess.run( ["youtube-dl", "-e", "--get-id", link[1]], stdout=subprocess.PIPE, universal_newlines=True )
-            if complete.returncode == 0:
-                tokens = complete.stdout.split('\n')
-                self._qlock.acquire()
-                self._q.append(tokens)
-                #print("start")
-                #for item in self._q:
-                #    print("{} = {}".format(item[0], item[1]))
-                self._qlock.release()
+            self._callbacks[parsed_json['cmd']](sock, parsed_json)
 
-            # TODO: return an error if url is malformed
-            q = []
-            for vid in self._q:
-                q.append({"name" : vid[0], "id" : vid[1]})
-            msg = {"cmd" : yt_rpc.CMD_RSP_ADD_VIDEO, "videos" : q }
-            link[0].sendall(json.JSONEncoder().encode(msg).encode('utf-8'))
 
     def _hndlr_enqueue(self, sock, parsed_json):
         """
-        Add a video. This will first add the link to the thread job queue where
-        a thread will do the work of requesting video metatadata and then adding
-        that data to the true video queue.
+        Add a video to queue.
 
         :param parsed_json: Received json message
         :type parsed_json: parsed json object
         """
-        link = (sock, parsed_json['link'])
-        self._jobcv.acquire()
-        self._jobq.append(link)
-        self._jobcv.notify()
-        self._jobcv.release()
+        link = parsed_json['link']
+
+        complete = subprocess.run( ["youtube-dl", "-e", "--get-id", link], stdout=subprocess.PIPE, universal_newlines=True )
+        if complete.returncode == 0:
+            tokens = complete.stdout.split('\n')
+            self._qlock.acquire()
+            self._q.append(tokens)
+            self._qlock.release()
+            self._log.write("1. [{}](https://www.youtube.com/watch?v={})\n".format(tokens[0], tokens[1]))
+            self._log.flush()
+
+        # TODO: return an error if url is malformed
+        q = []
+        for vid in self._q:
+            q.append({"name" : vid[0], "id" : vid[1]})
+        msg = {"cmd" : yt_rpc.CMD_RSP_ADD_VIDEO, "videos" : q }
+        sock.sendall(json.JSONEncoder().encode(msg).encode('utf-8'))
 
     def _hndlr_get_now_playing(self, sock, parsed_json):
         """
@@ -147,7 +141,10 @@ class yt_player:
             parsed_json = json.loads(msg)
             if 'cmd' in parsed_json:
                 if parsed_json['cmd'] in self._callbacks:
-                    self._callbacks[parsed_json['cmd']](sock, parsed_json)
+                    self._jobcv.acquire()
+                    self._jobq.append((sock, parsed_json))
+                    self._jobcv.notify()
+                    self._jobcv.release()
                 else:
                     print('No callback for "{}"'.format(parsed_json['cmd']))
             else:
--- frontend/static/behavior.js
+++ frontend/static/behavior.js
@@ -14,23 +14,23 @@ $(document).ready(function(){
             $("#queue_container").append(data);
             $("#submit_btn").prop("disabled", false);
             $("#submit_btn").text("Submit Link");
+            $("#queue_title").click(refresh_elements);
+            $("#queue_title").on("tap", refresh_elements);
         });
     });
 
     // refreshes the now playing banner and queue
     function refresh_elements() {
+        $.get("/now_playing", function(data, status){
+            $("#np_song").empty();
+            $("#np_song").append(data);
+        });
+
         $.get("/queue", function(data, status){
             $("#queue_container").empty();
             $("#queue_container").append(data);
             $("#queue_title").click(refresh_elements);
             $("#queue_title").on("tap", refresh_elements);
-        });
-
-        $.get("/now_playing", function(data, status){
-            $("#np_song").empty();
-            $("#np_song").append(data);
-            $("#np").click(refresh_elements);
-            $("#np").on("tap", refresh_elements);
         });
     };
 
